2496e12148afca1ddcce82a5d6858316c80cc5c3
Sergey Vdovareize <sergey.vdovareize@inspera.no>
2022-08-10 11:25:36 +0300

    params support

--- map.js
+++ map.js
@@ -1,19 +1,49 @@
 const page = require('webpage').create();
 const args = require('system').args.slice(1);
+const fs = require('fs');
 
-page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
-page.viewportSize = {
-  width: 2000,
-  height: 2000
-}
+const baseUrl = 'file:///' + fs.workingDirectory + '/webapp/index.html'
+const nonUrlParams = ['size']
 
-const baseUrl = 'file:///G:/work/leafletjs-api-tester/webapp/index.html'
-
-function gerParams() {
+function parseParams() {
   const paramsArray = args.filter(function(arg) {
     return arg.indexOf('=') !== -1
   });
-  return paramsArray.length > 0 ? paramsArray.join('&') : ''
+
+  const parsedParams = {}
+  paramsArray.forEach(function(paramString) {
+    const keyValue = paramString.split('=')
+    parsedParams[keyValue[0]] = keyValue[1]
+  })
+
+  return parsedParams
+}
+
+function composeUrlParams(params) {
+  const stringParams = Object.keys(params)
+    .filter(function(param) {
+      return nonUrlParams.indexOf(param) === -1
+    }).map(function(param) {
+      return param + '=' + params[param]
+    })
+
+  return stringParams.join('&')
+}
+
+function setViewportSize(parsedSize) {
+  var width = 1000;
+  var height = 1000;
+
+  const size = parsedSize.split(/,|x/)
+  if (!isNaN(size[0]) && !isNaN(size[1])) {
+    width = size[0]
+    height = size[1]
+  }
+  
+  page.viewportSize = {
+    width: width,
+    height: height,
+  }
 }
 
 function render(url) {
@@ -23,15 +53,17 @@ function render(url) {
         phantom.exit();
     } else {
         window.setTimeout(function () {
-            page.render('image.png', { quality: '30' });
+            page.render('image.png');
             phantom.exit();
         }, 1000);
     }
   });
 }
 
-
-const url = baseUrl + '?' + gerParams()
+const parsedParams = parseParams()
+const urlParams = composeUrlParams(parsedParams)
+const url = urlParams.length > 0 ? baseUrl + '?' + urlParams : baseUrl
+setViewportSize(parsedParams.size)
 render(url)
 
-// phantomjs map.js target=49.566115,25.576170 position=49.551971,25.59374 pilot=49.554738,25.607933 zoom=10
+// phantomjs map.js target=49.566115,25.576170 position=49.551971,25.59374 pilot=49.554738,25.607933 zoom=10 size=1000,1000
